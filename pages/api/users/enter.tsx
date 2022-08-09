ad2a7b16340bdd89542b1c3dc8ed27b31c25b8ce
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-08-09 19:08:02 +0900

    사용자 로그인 로직 추가 sms, email 두 가지외 IronSession 사용하여 세션 추가

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,62 @@
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.status(200).end();
+mail.setApiKey(process.env.SENDGRID_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) {
+    res.json({ ok: false });
+  }
+
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+
+  if (phone) {
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MY_PHONE!,
+    //   body: `Your login token is ${payload}.`,
+    // });
+    // console.log(message);
+  } else if (email) {
+    // const email = await mail.send({
+    //   from: "nico@nomadcoders.co",
+    //   to: "jeongh1021@naver.com",
+    //   subject: "Your Carrot Market Verification Email",
+    //   text: `Your token is ${payload}`,
+    //   html: `<strong>Your token is ${payload}</strong>`,
+    // });
+    // console.log(email);
+  }
+
+  console.log(token);
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
