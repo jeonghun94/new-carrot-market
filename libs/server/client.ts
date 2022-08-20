9804c17c9f64d6efd0c458affae1977a64e1fa77
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-08-20 15:32:55 +0900

    프리즈마 쿼리 로그 제외

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -3,7 +3,8 @@ declare global {
   var client: PrismaClient | undefined;
 }
 
-const client = global.client || new PrismaClient({ log: ["query"] });
+// const client = global.client || new PrismaClient({ log: ["query"] });
+const client = global.client || new PrismaClient();
 if (process.env.NODE_ENV === "development") global.client = client;
 
 export default client;
