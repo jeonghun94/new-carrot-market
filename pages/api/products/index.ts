cc1d084c41cb5cff36ca787c66a26bcb91d12fe3
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-08-22 14:27:11 +0900

    이미지 업로드, 읽기  완료

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -22,7 +22,7 @@ async function handler(
 
   if (req.method === "POST") {
     const {
-      body: { name, price, description },
+      body: { name, price, description, photoId },
       session: { user },
     } = req;
 
@@ -31,7 +31,7 @@ async function handler(
         name,
         price: +price,
         description,
-        image: "xxx",
+        image: photoId,
         user: { connect: { id: user?.id } },
       },
     });
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -34,6 +34,10 @@ const EditProfile: NextPage = () => {
     if (user?.name) setValue("name", user.name);
     if (user?.email) setValue("email", user.email);
     if (user?.phone) setValue("phone", user.phone);
+    if (user?.avatar)
+      setAvatarPreview(
+        `https://imagedelivery.net/jhi2XPYSyyyjQKL_zc893Q/${user?.avatar}/avatar`
+      );
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
@@ -45,19 +49,23 @@ const EditProfile: NextPage = () => {
       });
     }
     if (avatar && avatar.length > 0 && user) {
-      const { id, uploadURL } = await (await fetch(`/api/files`)).json();
+      const { uploadURL } = await (await fetch(`/api/files`)).json();
       const form = new FormData();
       form.append("file", avatar[0], user?.id + "");
-      await fetch(uploadURL, {
-        method: "POST",
-        body: form,
-      });
-      return;
+
+      const {
+        result: { id: avatarId },
+      } = await (
+        await fetch(uploadURL, {
+          method: "POST",
+          body: form,
+        })
+      ).json();
       editProfile({
         email,
         phone,
         name,
-        // avatarUrl: CF URL
+        avatarId,
       });
     } else {
       editProfile({
