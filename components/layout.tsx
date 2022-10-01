e5013096a9517c8d0d7191f664f4f07ca568c741
jeonghun94 <jeongh1021@naver.com>
2022-10-01 18:36:55 +0900

    나의 당근 페이지 UI수정

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,6 +10,7 @@ interface LayoutProps {
   hasTabBar?: boolean;
   children: React.ReactNode;
   seoTitle?: string;
+  setting?: boolean;
   // actionBtn?: React.ReactNode;
   noActionBar?: boolean;
 }
@@ -20,6 +21,7 @@ export default function Layout({
   hasTabBar,
   children,
   seoTitle,
+  setting,
   // actionBtn,
   noActionBar,
 }: LayoutProps) {
@@ -36,7 +38,11 @@ export default function Layout({
         <title>{headTitle}</title>
       </Head>
       {noActionBar ? null : (
-        <div className=" w-full h-14 max-w-xl px-10 flex justify-center items-center fixed top-0 text-lg font-medium text-gray-800  bg-white border-b">
+        <div
+          className={`w-full h-14 max-w-xl px-6 flex  ${
+            setting ? "justify-between" : "justify-center"
+          } items-center fixed top-0 text-lg font-medium text-gray-800  bg-white border-b`}
+        >
           {canGoBack ? (
             <button onClick={onClick} className="absolute left-4">
               <svg
@@ -55,6 +61,32 @@ export default function Layout({
               </svg>
             </button>
           ) : null}
+
+          {setting ? (
+            <button onClick={onClick} className="absolute right-4">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+              </svg>
+            </button>
+          ) : null}
+
           {title ? (
             <span className={cls(canGoBack ? "mx-auto" : "", "")}>{title}</span>
           ) : null}
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -7,6 +7,8 @@ import { Review, User } from "@prisma/client";
 import { cls } from "@libs/client/utils";
 import { withSsrSession } from "@libs/server/withSession";
 import client from "@libs/server/client";
+import logo from "public/carrot-logo.png";
+import Image from "next/image";
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -21,95 +23,199 @@ const Profile: NextPage = () => {
   const { user } = useUser();
   const { data } = useSWR<ReviewsResponse>("/api/reviews");
   return (
-    <Layout hasTabBar title="나의 캐럿">
-      <div className="px-4">
-        <div className="flex items-center mt-4 space-x-3">
-          {user?.avatar ? (
-            <img
-              src={`https://imagedelivery.net/jhi2XPYSyyyjQKL_zc893Q/${user?.avatar}/avatar`}
-              className="w-16 h-16 bg-slate-500 rounded-full"
-            />
-          ) : (
-            <div className="w-16 h-16 bg-slate-500 rounded-full" />
-          )}
-          <div className="flex flex-col">
-            <span className="font-medium text-gray-900">{user?.name}</span>
-            <Link href="/profile/edit">
-              <a className="text-sm text-gray-700">Edit profile &rarr;</a>
-            </Link>
+    <Layout setting title="나의 당근" hasTabBar>
+      <div className="mt-2 px-4">
+        <div className="w-full flex justify-between items-center py-4 space-x-3">
+          <div className="flex gap-3">
+            {user?.avatar ? (
+              <img
+                src={`https://imagedelivery.net/jhi2XPYSyyyjQKL_zc893Q/${user?.avatar}/avatar`}
+                className="w-16 h-16 bg-slate-500 rounded-full"
+              />
+            ) : (
+              <div className="w-12 h-12 flex justify-center items-center rounded-full bg-gray-300 text-3xl">
+                🙎🏻‍♂️
+              </div>
+            )}
+            <div className="flex flex-col">
+              <h3 className="font-medium text-gray-900">{user?.name}</h3>
+              <h5 className="text-sm text-gray-700">신월7동 #5498035</h5>
+            </div>
+          </div>
+          <div>
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+              ></path>
+            </svg>
           </div>
         </div>
-        <div className="mt-10 flex justify-around">
-          <Link href="/profile/sold">
-            <a className="flex flex-col items-center">
-              <div className="w-14 h-14 text-white bg-orange-400 rounded-full flex items-center justify-center">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  ></path>
-                </svg>
-              </div>
-              <span className="text-sm mt-2 font-medium text-gray-700">
-                판매내역
-              </span>
-            </a>
+        <div className="w-full mb-3 p-3 flex justify-between items-center border-2 border-dashed border-orange-500 rounded-md">
+          <div className="flex items-center gap-1 text-orange-500 font-extrabold">
+            <div>
+              <Image src={logo} width={13} height={15}></Image>
+            </div>
+            PAY
+          </div>
+          <div className="text-sm text-gray-500 font-semibold">
+            중고거래는 이제 당근페이로 해보세요!
+          </div>
+        </div>
+
+        <div className="mb-3 pb-3 border-b">
+          <div className="py-1 text-lg font-semibold">나의 거래</div>
+          <Link href={"/profile/sold"}>
+            <div className="py-3 flex items-center gap-3 cursor-pointer">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                ></path>
+              </svg>
+              <div className="font-normal">판매내역</div>
+            </div>
           </Link>
-          <Link href="/profile/bought">
-            <a className="flex flex-col items-center">
-              <div className="w-14 h-14 text-white bg-orange-400 rounded-full flex items-center justify-center">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                  ></path>
-                </svg>
-              </div>
-              <span className="text-sm mt-2 font-medium text-gray-700">
-                구매내역
-              </span>
-            </a>
+          <Link href={"/profile/bought"}>
+            <div className="py-3 flex items-center gap-3 cursor-pointer">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                ></path>
+              </svg>
+              <div className="font-normal">구매내역</div>
+            </div>
           </Link>
-          <Link href="/profile/loved">
-            <a className="flex flex-col items-center">
-              <div className="w-14 h-14 text-white bg-orange-400 rounded-full flex items-center justify-center">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  ></path>
-                </svg>
-              </div>
-              <span className="text-sm mt-2 font-medium text-gray-700">
-                관심목록
-              </span>
-            </a>
+          <Link href={"/profile/loved"}>
+            <div className="py-3 flex items-center gap-3 cursor-pointer">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              <div className="font-normal">관심목록</div>
+            </div>
+          </Link>
+          <div className="py-3 flex items-center gap-3 cursor-pointer">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+              ></path>
+            </svg>
+            <div className="font-normal">당근가계부</div>
+          </div>
+        </div>
+
+        <div className="mb-3 pb-3 border-b">
+          <div className="py-1 text-lg font-semibold">나의 동네생활</div>
+          <div className="py-3 flex items-center gap-3 cursor-pointer">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+              ></path>
+            </svg>
+            <div className="font-normal">동네생활 글/댓글</div>
+          </div>
+        </div>
+
+        <div className="mb-3 pb-3 border-b">
+          <div className="py-1 text-lg font-semibold">기타</div>
+          <div className="py-3 flex items-center gap-3 cursor-pointer">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              ></path>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              ></path>
+            </svg>
+            <div className="font-normal">내 동네 설정</div>
+          </div>
+          <Link href={"/profile/loved"}>
+            <div className="py-3 flex items-center gap-3 cursor-pointer">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <div className="font-normal">동네 인증하기</div>
+            </div>
           </Link>
         </div>
+
         {data?.reviews.map((review) => (
           <div key={review.id} className="mt-6">
             <div className="flex space-x-4 items-center">
