cef6b9be6b01a70ded8a5b49b6f78fadebd98c76
조정훈 <jeongh1021@naver.com>
2023-05-12 15:55:56 +0900

    프로필 위치 설정 비활성화

--- pages/profile/map.tsx
+++ pages/profile/map.tsx
@@ -38,59 +38,61 @@ const IndexPage: NextPage = () => {
   //   }
   // }, []);
 
-  const { myLocation } = useMap({
-    userLatitude: Number(userLatitude),
-    userLongitude: Number(userLongitude),
-    userAddress: userAddress + "",
-  });
+  // const { myLocation } = useMap({
+  //   userLatitude: Number(userLatitude),
+  //   userLongitude: Number(userLongitude),
+  //   userAddress: userAddress + "",
+  // });
 
-  const handleOnClick = () => {
-    if (confirm("현재 위치를 동네로 설정하시겠습니까?")) {
-      location({ myLocation });
-      if (data && data.ok) {
-        alert("동네가 설정되었습니다.");
-        router.push("/profile");
-      }
-    }
-  };
+  // const handleOnClick = () => {
+  //   if (confirm("현재 위치를 동네로 설정하시겠습니까?")) {
+  //     location({ myLocation });
+  //     if (data && data.ok) {
+  //       alert("동네가 설정되었습니다.");
+  //       router.push("/profile");
+  //     }
+  //   }
+  // };
 
-  return (
-    <Layout seoTitle="동네 인증" title="동네 인증하기" actionBar backBtn>
-      <div className="w-full h-96" id="map"></div>
-      <div className="p-3 bg-orange-500 text-white text-sm text-center">
-        <p>
-          {myLocation.isSame
-            ? `현재 위치가 내 동네로 설정한 <sapn className="text-sm font-semibold">${userAddress}</sapn>에 있습니다.`
-            : `잠깐만요! 현재 위치가 ${myLocation.address} 이에요.`}
-        </p>
-      </div>
-      <div className="p-2">
-        {myLocation.isSame ? (
-          <>
-            <button
-              className="w-full mt-2 p-2 text-center text-sm font-semibold rounded-md border bg-orange-500 text-white"
-              onClick={() => router.push("/profile")}
-            >
-              동네인증 완료하기
-            </button>
-          </>
-        ) : (
-          <>
-            <p className="p-2 text-sm text-center">
-              현재 내 동네로 설정되어 있는 동네서만 동네인증을 할 수 있어요.
-              현재 위치를 확인해 주세요.
-            </p>
-            <button
-              className="w-full mt-2 p-2 text-center text-sm font-semibold rounded-md border"
-              onClick={handleOnClick}
-            >
-              현재 위치로 동네 변경하기
-            </button>
-          </>
-        )}
-      </div>
-    </Layout>
-  );
+  // return (
+  //   <Layout seoTitle="동네 인증" title="동네 인증하기" actionBar backBtn>
+  //     <div className="w-full h-96" id="map"></div>
+  //     <div className="p-3 bg-orange-500 text-white text-sm text-center">
+  //       <p>
+  //         {myLocation.isSame
+  //           ? `현재 위치가 내 동네로 설정한 <sapn className="text-sm font-semibold">${userAddress}</sapn>에 있습니다.`
+  //           : `잠깐만요! 현재 위치가 ${myLocation.address} 이에요.`}
+  //       </p>
+  //     </div>
+  //     <div className="p-2">
+  //       {myLocation.isSame ? (
+  //         <>
+  //           <button
+  //             className="w-full mt-2 p-2 text-center text-sm font-semibold rounded-md border bg-orange-500 text-white"
+  //             onClick={() => router.push("/profile")}
+  //           >
+  //             동네인증 완료하기
+  //           </button>
+  //         </>
+  //       ) : (
+  //         <>
+  //           <p className="p-2 text-sm text-center">
+  //             현재 내 동네로 설정되어 있는 동네서만 동네인증을 할 수 있어요.
+  //             현재 위치를 확인해 주세요.
+  //           </p>
+  //           <button
+  //             className="w-full mt-2 p-2 text-center text-sm font-semibold rounded-md border"
+  //             onClick={handleOnClick}
+  //           >
+  //             현재 위치로 동네 변경하기
+  //           </button>
+  //         </>
+  //       )}
+  //     </div>
+  //   </Layout>
+  // );
+
+  return <div>d</div>;
 };
 
 export default IndexPage;
